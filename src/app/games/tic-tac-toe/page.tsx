8ac4ffc4929eebe0a94b2a72d640a1c8caaa0f8d
AtheeshRathnaweera <atheesh.rathnaweera@novigi.com.au>
2024-10-26 18:40:49 +0530

    feat: created pages for games sections

---
+++ src/app/games/tic-tac-toe/page.tsx
@@ -0,0 +1,16 @@
+// `app/tictactoe/page.tsx` is the UI for the `/tictactoe` URL
+// import { useState } from "react";
+
+export default function Page() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hello from Tic Tac Toe page!</h1>
+      {/* <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </div> */}
+    </>
+  )
+}
