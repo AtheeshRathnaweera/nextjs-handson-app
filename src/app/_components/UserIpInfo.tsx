fc5dcaf8bee57a9221b0c8c43959b1ac5c862db4
AtheeshRathnaweera <atheesh.rathnaweera@novigi.com.au>
2024-11-01 14:03:33 +0530

    feat: show the user's info from the IP under top nav bar

--- src/app/_components/UserIpInfo.tsx
+++ src/app/_components/UserIpInfo.tsx
@@ -1,50 +1,44 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { IpInfoResponse } from "../api/_dto/IpInfoResponse";
 
 export default function UserIpInfo() {
-  const [ip, setIp] = useState('');
+  const [ipInfo, setIpInfo] = useState<IpInfoResponse | null>(null);
 
-  // get the user IP when loading
   useEffect(() => {
-    fetch("https://api.ipify.org")
-      .then((res) => res.text())
-      .then((data) => {
-        setIp(data);
-        console.log("This is the client IP 01:", data);
+    const fetchIpData = async () => {
+      // API call to get the current user IP
+      const userIpResponse = await fetch("https://api.ipify.org")
+      if (!userIpResponse.ok) {
+        throw new Error("Failed to fetch users's IP");
+      }
+      const userIp = await userIpResponse.text();
+
+      // API call to retrieve more information from the user IP
+      const ipInfoResponse = await fetch("/api/IpInfo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ip: userIp }),
       });
+      if (!ipInfoResponse.ok) {
+        throw new Error("Failed to fetch users's IP information");
+      }
+      const userIpInfo = await ipInfoResponse.json();
+      setIpInfo(userIpInfo)
+    }
+
+    fetchIpData();
   }, []); 
   
 
   return (
     <nav className="border-b border-gray-300 dark:border-gray-600">
       <div className="container flex items-center justify-center mx-auto capitalize p-3 text-sm">
-        { ip + " | " }Colombo, Sri Lanka | +05:30
+        { ipInfo?.ip + " | " + ipInfo?.city_name + ", " + ipInfo?.country_name + " | " + ipInfo?.time_zone }
       </div>
-      {/* <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4 border-red-200">
-        <a
-          href="https://flowbite.com"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
-        </a>
-        <div className="flex items-center space-x-6 rtl:space-x-reverse">
-          <a
-            href="tel:5541251234"
-            className="text-sm dark:text-white hover:underline"
-          >
-            (555) 412-1234
-          </a>
-          <a
-            href="#"
-            className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            Login
-          </a>
-        </div>
-      </div> */}
     </nav>
   );
 }
