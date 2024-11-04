c8a48f3fc7a810fa5214a243a228f39a762bcf0b
AtheeshRathnaweera <atheesh.rathnaweera@novigi.com.au>
2024-11-05 01:52:54 +0530

    feat: update the api route handlers with custom error classes

--- src/app/api/ip-info/route.ts
+++ src/app/api/ip-info/route.ts
@@ -1,6 +1,11 @@
 import { IpInfoRequest } from '../_dto/IpInfoRequest';
 import { plainToInstance } from 'class-transformer';
 import { IpInfoResponse } from '../_dto/IpInfoResponse';
+import { BadRequestException } from '../_exceptions/BadRequestException';
+import { HttpException } from '../_exceptions/HttpException';
+import { HttpErrorCode } from '@/app/_constants/HttpErrorCode';
+import { NextResponse } from 'next/server';
+import { ExternalApiException } from '../_exceptions/ExternalApiException';
 
 export async function POST(request: Request) {
   try {
@@ -8,23 +13,26 @@ export async function POST(request: Request) {
     const validatedData = plainToInstance(IpInfoRequest, requestData);
 
     if (!validatedData.ip) {
-      throw new Error('ip is invalid');
+      throw new BadRequestException('The IP property is required and must be provided.');
     }
     
     const apiUrl = `https://api.ip2location.io/?key=${process.env.IP2LOCATION_API_KEY}&ip=${validatedData.ip}`;
     const response = await fetch(apiUrl);
 
     if (!response.ok) { 
-      throw new Error('Failed to fetch IP data');
+      throw new ExternalApiException('Failed to retrieve data from ip2location API', response.status);
     }
 
     const data = await response.json();
     const ipInfoResponse = plainToInstance(IpInfoResponse, data);
-    return Response.json(ipInfoResponse)
+
+    return NextResponse.json(ipInfoResponse);
   } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: 'Failed to fetch IP data. Error occurred.' }), {
-      status: 500,
-    });
+    console.error("Error fetching user's ip info:", error);
+
+    if (error instanceof HttpException) {
+      return NextResponse.json({ error: error.message, errorCode: error.errorCode, errors: error.errors }, { status: error.statusCode });
+    }
+    return NextResponse.json({ error: 'Internal Server Error', errorCode: HttpErrorCode.FETCH_FAILED }, { status: 500 });
   }
 }
