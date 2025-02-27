d59fbeb389aba354f713b24678d01b2e05e85470
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-02-27 12:06:58 +0530

    feat: update the home page new design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,28 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-geist-sans)', 'sans-serif'], // Add your custom font here
       },
+      keyframes: {
+        typing: {
+          '0%': {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+      }
       // colors: {
       //   background: "var(--background)",
       //   foreground: "var(--foreground)",
