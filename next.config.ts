59ef9894403b78413a21f0df5c2d68b130995a2d
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-02-27 12:06:58 +0530

    feat: add images domain to the configs file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['i.pinimg.com', 'cdn-icons-png.flaticon.com'], // Replace with your image source domain
+  },
 };
 
 export default nextConfig;
