59665558018c6fe8f66249f27ee40514fc064e05
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-02-27 12:06:58 +0530

    feat: add entry to api providers section in the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,23 @@
 import "server-only";
 
 export default function Page() {
-  return <div>This is the about page</div>;
+  return (
+    <div className="flex flex-col items-center p-8">
+      <main className="flex flex-col items-center max-w-2xl mx-auto">
+        <h1 className="text-3xl font-semibold mb-4">About</h1>
+        <h2 className="text-xl font-medium mb-2">Data Sources</h2>
+        <h3 className="font-medium mb-2">API Providers:</h3>
+        <p className="text-base">
+          This application uses IP2Location.io{" "}
+          <a
+            href="https://www.ip2location.io"
+            className="text-blue-500 underline"
+          >
+            IP geolocation
+          </a>{" "}
+          web service.
+        </p>
+      </main>
+    </div>
+  );
 }
